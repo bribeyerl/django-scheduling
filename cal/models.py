bb422a1cb567651aedd58ff02768e0dee36058a7
bribeyerl <briabeyerl@gmail.com>
2022-11-25 21:11:09 -0500

    updated models for dropdown menu on scheduling form

--- cal/models.py
+++ cal/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+ASSIGNED_TO = [
+    ('BR', 'Brad'),
+    ('MA', 'Matt'),
+    ('NI', 'Nick'),
+    ('OT', 'Other'),
+]
 
 class Form(models.Model):
     first_name = models.CharField(max_length=200)
@@ -9,5 +15,5 @@ class Form(models.Model):
     city = models.CharField(max_length=50)
     zip = models.CharField(max_length=5)
     job_details = models.CharField(max_length=500)
-    assignee = models.CharField(max_length=200)
+    assignee = models.CharField(max_length=10, choices=ASSIGNED_TO, default='other')
     sched_on = models.DateTimeField('scheduled on')
